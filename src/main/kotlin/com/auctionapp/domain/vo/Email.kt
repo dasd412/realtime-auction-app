82d2ac0160d8cda5a8fbc4a09974367ace05becb
yang-yeongjun <dasd412@naver.com>
2025-06-19 11:29:47 +0900

    feat : VO 추가

---
+++ src/main/kotlin/com/auctionapp/domain/vo/Email.kt
@@ -0,0 +1,14 @@
+package com.auctionapp.domain.vo
+
+import com.auctionapp.com.auctionapp.domain.exception.InvalidEmailException
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Email(val value: String) {
+    init {
+        val pattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+        if (!Regex(pattern).matches(value)) {
+            throw InvalidEmailException()
+        }
+    }
+}
---
+++ src/main/kotlin/com/auctionapp/domain/vo/Money.kt
@@ -0,0 +1,17 @@
+package com.auctionapp.domain.vo
+
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Money(val amount: Long) {
+    init {
+        require(amount >= 0) {
+            "금액은 0 이상이어야 합니다."
+        }
+    }
+
+    fun add(other: Money): Money = Money(this.amount + amount)
+    fun subtract(other: Money): Money = Money(this.amount - amount)
+    fun isGreaterThan(other: Money): Boolean = this.amount > other.amount
+    fun isGreaterThanOrEqual(other: Money): Boolean = this.amount >= other.amount
+}
