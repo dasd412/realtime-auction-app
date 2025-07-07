b96c57f85c1a2c246cba5a7dfaf43ac56a552097
yang-yeongjun <dasd412@naver.com>
2025-07-07 16:15:44 +0900

    feat : 스프링 시큐리티 도입으로 경매 서비스 메서드 수정

--- src/main/kotlin/com/auctionapp/application/service/AuctionAppService.kt
+++ src/main/kotlin/com/auctionapp/application/service/AuctionAppService.kt
@@ -4,12 +4,15 @@ import com.auctionapp.application.constant.DEFAULT_AUCTION_PAGE_SIZE
 import com.auctionapp.application.exception.NotFoundAuctionException
 import com.auctionapp.application.exception.NotFoundProductException
 import com.auctionapp.application.exception.NotFoundUserException
+import com.auctionapp.application.exception.UnauthorizedException
 import com.auctionapp.com.auctionapp.expriment.concurrency.ConcurrencyControlStrategyRegistry
+import com.auctionapp.com.auctionapp.utils.SecurityUtil
 import com.auctionapp.domain.entity.Auction
 import com.auctionapp.domain.entity.AuctionStatus
 import com.auctionapp.domain.entity.Bid
 import com.auctionapp.domain.entity.User
 import com.auctionapp.domain.service.AuctionService
+import com.auctionapp.domain.vo.Email
 import com.auctionapp.domain.vo.Money
 import com.auctionapp.infrastructure.persistence.*
 import org.redisson.api.RedissonClient
@@ -17,6 +20,7 @@ import org.redisson.client.RedisException
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Service
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.annotation.Propagation
@@ -46,17 +50,21 @@ class AuctionAppService(
     private val redissonClient: RedissonClient,
     private val transactionManager: PlatformTransactionManager,
 ) {
+    @PreAuthorize("hasRole('CUSTOMER')")
     @Transactional
     fun registerAuction(
-        userId: Long,
         productId: Long,
         initialPrice: Long,
         minimumBidUnit: Long,
         startTime: LocalDateTime,
         endTime: LocalDateTime,
     ) {
-        val user = userRepository.findByIdOrNull(userId) ?: throw NotFoundUserException()
+        val username = SecurityUtil.getCurrentUsername() ?: throw UnauthorizedException()
+
+        val user = userRepository.findByEmail(Email(username)) ?: throw NotFoundUserException()
+
         val product = productRepository.findByIdOrNull(productId) ?: throw NotFoundProductException()
+
         val auction =
             Auction(
                 initialPrice = Money(initialPrice),
@@ -103,23 +111,25 @@ class AuctionAppService(
         return auctionRepository.findAuctionDetailById(auctionId) ?: throw NotFoundAuctionException()
     }
 
+    @PreAuthorize("hasRole('CUSTOMER')")
     @Transactional
     fun cancelAuction(
-        userId: Long,
         auctionId: Long,
     ) {
-        val user = userRepository.findByIdOrNull(userId) ?: throw NotFoundUserException()
+        val username = SecurityUtil.getCurrentUsername() ?: throw UnauthorizedException()
+        val user = userRepository.findByEmail(Email(username)) ?: throw NotFoundUserException()
         val auction = auctionRepository.findByIdOrNull(auctionId) ?: throw NotFoundAuctionException()
         auctionService.cancelAuction(auction, user)
     }
 
+    @PreAuthorize("hasRole('CUSTOMER')")
     @Transactional
     fun placeBid(
-        userId: Long,
         auctionId: Long,
         amount: Long,
     ): Long {
-        val user = userRepository.findByIdOrNull(userId) ?: throw NotFoundUserException()
+        val username = SecurityUtil.getCurrentUsername() ?: throw UnauthorizedException()
+        val user = userRepository.findByEmail(Email(username)) ?: throw NotFoundUserException()
         val auction = auctionRepository.findByIdOrNull(auctionId) ?: throw NotFoundAuctionException()
         val money = Money(amount)
 
@@ -137,8 +147,8 @@ class AuctionAppService(
     // synchronized, tryLock, semaphore는 분산 서버에선 동시성 제어가 안되고
     // 비관적 락은 분산 DB에서 동시성 제어가 안되므로 레디스 분산 락을 활용함.
     // 분산락과 트랜잭션을 함께 사용할 경우, 락 획득 → 트랜잭션 시작 → 비즈니스 로직 → 트랜잭션 커밋 또는 롤백 → 락 해제 순서로 진행되야 함
+    @PreAuthorize("hasRole('CUSTOMER')")
     fun placeBidWithRedisLock(
-        userId: Long,
         auctionId: Long,
         amount: Long,
     ): Long {
@@ -168,7 +178,8 @@ class AuctionAppService(
             // - TransactionTemplate에서는 예외 발생으로 롤백되더라도 finally 블록에서 락 해제가 보장됩니다.
             // - @Transactional에서는 예외 시 트랜잭션 롤백과 락 해제 순서가 명확하지 않을 수 있습니다.
             return transactionTemplate.execute {
-                val user = userRepository.findByIdOrNull(userId) ?: throw NotFoundUserException()
+                val username = SecurityUtil.getCurrentUsername() ?: throw UnauthorizedException()
+                val user = userRepository.findByEmail(Email(username)) ?: throw NotFoundUserException()
                 val auction = auctionRepository.findByIdOrNull(auctionId) ?: throw NotFoundAuctionException()
                 val money = Money(amount)
 
@@ -211,30 +222,33 @@ class AuctionAppService(
         )
     }
 
+    @PreAuthorize("hasRole('CUSTOMER')")
     @Transactional(readOnly = true)
     fun getBidsOfUser(
-        userId: Long,
         pageNumber: Int,
     ): Page<Bid> {
-        val user = userRepository.findByIdOrNull(userId) ?: throw NotFoundUserException()
+        val username = SecurityUtil.getCurrentUsername() ?: throw UnauthorizedException()
+        val user = userRepository.findByEmail(Email(username)) ?: throw NotFoundUserException()
         return bidRepository.findByUserOrderByCreatedAtDesc(user, PageRequest.of(pageNumber, DEFAULT_AUCTION_PAGE_SIZE))
     }
 
+    @PreAuthorize("hasRole('CUSTOMER')")
     @Transactional(readOnly = true)
     fun getAuctionsOfAuctionOwner(
-        userId: Long,
         pageNumber: Int,
     ): Page<Auction> {
-        val user = userRepository.findByIdOrNull(userId) ?: throw NotFoundUserException()
+        val username = SecurityUtil.getCurrentUsername() ?: throw UnauthorizedException()
+        val user = userRepository.findByEmail(Email(username)) ?: throw NotFoundUserException()
         return auctionRepository.findByUser(user, PageRequest.of(pageNumber, DEFAULT_AUCTION_PAGE_SIZE))
     }
 
+    @PreAuthorize("hasRole('CUSTOMER')")
     @Transactional(readOnly = true)
     fun getAuctionsOfBidder(
-        userId: Long,
         pageNumber: Int,
     ): Page<Auction> {
-        val user = userRepository.findByIdOrNull(userId) ?: throw NotFoundUserException()
+        val username = SecurityUtil.getCurrentUsername() ?: throw UnauthorizedException()
+        val user = userRepository.findByEmail(Email(username)) ?: throw NotFoundUserException()
         return auctionRepository.findByBidUser(user, PageRequest.of(pageNumber, DEFAULT_AUCTION_PAGE_SIZE))
     }
 }
