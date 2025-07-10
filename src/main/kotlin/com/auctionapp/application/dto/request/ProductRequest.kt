c4756b54c162842eac09fe456d7f64b6ec7ddfe9
yang-yeongjun <dasd412@naver.com>
2025-07-10 11:38:34 +0900

    feat : request dto 추가

---
+++ src/main/kotlin/com/auctionapp/application/dto/request/ProductRequest.kt
@@ -0,0 +1,23 @@
+package com.auctionapp.application.dto.request
+
+import jakarta.validation.constraints.NotBlank
+
+data class RegisterProductRequest(
+    @field:NotBlank(message = "상품명은 필수입니다.")
+    val name: String,
+
+    val description: String?,
+
+    @field:NotBlank(message = "이미지 URL은 필수입니다.")
+    val imageUrl: String
+)
+
+data class UpdateProductRequest(
+    @field:NotBlank(message = "상품명은 필수입니다.")
+    val name: String,
+
+    val description: String?,
+
+    @field:NotBlank(message = "이미지 URL은 필수입니다.")
+    val imageUrl: String
+)
