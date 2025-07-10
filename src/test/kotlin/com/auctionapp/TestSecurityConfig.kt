ef671427f37b799156e3be903d71412754dc3d8d
yang-yeongjun <dasd412@naver.com>
2025-07-10 16:53:05 +0900

    fix : 필요없는 application 설정 삭제

--- src/test/kotlin/com/auctionapp/TestSecurityConfig.kt
+++ src/test/kotlin/com/auctionapp/TestSecurityConfig.kt
@@ -1,9 +1,11 @@
 package com.auctionapp
 
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 
+@TestConfiguration
 @SuppressWarnings("unused")
 class TestSecurityConfig {
     @Bean
