0f878cd46c7480dc2fe2233e0fc5bce85f9cfb6e
yang-yeongjun <dasd412@naver.com>
2025-07-13 15:39:46 +0900

    fix : 테스트 프로파일에서 redis 못 찾는 문제 수정

--- src/main/kotlin/AuctionAppApplication.kt
+++ src/main/kotlin/AuctionAppApplication.kt
@@ -1,18 +1,9 @@
 package com.auctionapp
 
-import org.redisson.spring.starter.RedissonAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
-import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    exclude = [
-        RedissonAutoConfiguration::class,
-        RedisAutoConfiguration::class,
-        RedisRepositoriesAutoConfiguration::class,
-    ],
-)
+@SpringBootApplication
 class AuctionAppApplication
 
 fun main(args: Array<String>) {
